91a5224a8e343baa81ede3b6f3914cdf3356fd58
Kyle Geppert <krgeppert@gmail.com>
2013-12-10 14:53:20 -0800

    Game logic written. Game will end when someone wins, ties when the board fills up, and speaks at you. MVP finished.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,46 @@
 angular.module('ticTacToeApp')
   .controller('MainCtrl', function ($scope, Board) {
     $scope.gameInProgress = false;
-    $scope.playerMark = 'X';
-    $scope.board = Board(3);
+    $scope.board;
+    $scope.gameOver = false;
+    var movesMade = 0;
 
     $scope.play = function(){
+      $scope.board = new Board(3)
+      $scope.gameOver = false;
       $scope.playerMark = 'X';
       $scope.gameInProgress = true;
     };
     $scope.toggle = function(y, x){
       $scope.board[y][x] = $scope.board[y][x] ? $scope.board[y][x] : $scope.playerMark;
-      $scope.playerMark = $scope.playerMark === 'X' ? 'O' : 'X';
+      if (movesMade++ === $scope.board.length * $scope.board.length) gameOver(false);
+      if (checkForVictory(y,x,$scope.playerMark)){
+        gameOver($scope.playerMark);
+      } else {
+        $scope.playerMark = $scope.playerMark === 'X' ? 'O' : 'X';
+      }
     };
-
+    function gameOver (winner){
+      $scope.gameInProgress = false;
+      $scope.gameOver = true;
+      if (winner){
+        $scope.endMessage = winner + '\'s Win!';
+      } else {
+        $scope.endMessage = 'Cat\'s Game.';
+      }
+    }
+    function checkForVictory (y,x, player){ // tested manually.
+      var l = $scope.board.length;
+      var horizontal = true;
+      var verticle = true;
+      var posDiagonal = true;
+      var negDiagonal = true;
+      for (var i = 0; i < l; i++){
+        if ($scope.board[y][i] !== player) horizontal = false;
+        if ($scope.board[i][x] !== player) verticle = false;
+        if ($scope.board[i][i] !== player) posDiagonal = false; // negative slope diagonal.
+        if ($scope.board[i][l - i - 1] !== player) negDiagonal = false; //positive slope diagonal.
+      }
+      return horizontal || verticle || posDiagonal || negDiagonal;
+    }
   });
